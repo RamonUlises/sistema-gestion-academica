e86b526623fde786bf94fe56bc8b53a46694991e
Moises Laguna <lagunamoises64@gmail.com>
2024-04-30 16:08:52 -0600

    pqnelRounded

---
+++ SGA/MBControl/PanelRounded.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SGA.MBControl
+{
+    public static class PanelHelper
+    {
+        public static void SetRoundPanel(Panel panel, int radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(0, 0, radius, radius, 180, 90);
+            path.AddArc(panel.Width - radius, 0, radius, radius, 270, 90);
+            path.AddArc(panel.Width - radius, panel.Height - radius, radius, radius, 0, 90);
+            path.AddArc(0, panel.Height - radius, radius, radius, 90, 90);
+            path.CloseFigure();
+            panel.Region = new Region(path);
+        }
+    }
+
+
+
+
+    // Uso en tu formulario:
+    // var roundedPanel = new RoundedPanel();
+    // roundedPanel.Size = new Size(200, 100);
+    // this.Controls.Add(roundedPanel);
+
+}
