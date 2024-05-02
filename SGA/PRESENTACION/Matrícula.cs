79ea4b10e32fcadb6ad6060d8859b665b4102687
Moises Laguna <lagunamoises64@gmail.com>
2024-05-01 23:12:26 -0600

    create registration form

--- SGA/PRESENTACION/Matrícula.cs
+++ SGA/PRESENTACION/Matrícula.cs
@@ -16,7 +16,31 @@ namespace SGA
         public Matrícula()
         {
             InitializeComponent();
-            
+            PanelHelper.SetRoundPanel(panel2, 25);
+            PanelHelper.SetRoundPanel(panel3, 25);
+            PanelHelper.SetRoundPanel(panel4, 25);
+            PanelHelper.SetRoundPanel(panel5, 25);
+            PanelHelper.SetRoundPanel(panel6, 25);  
+            PanelHelper.SetRoundPanel(panel7, 25);
+            PanelHelper.SetRoundPanel(panel8, 25);
+            PanelHelper.SetRoundPanel(panel9, 25);
+            PanelHelper.SetRoundPanel(panel10, 25);
+            PanelHelper.SetRoundPanel(panel11, 25);
+            PanelHelper.SetRoundPanel(panel12, 25);
+            PanelHelper.SetRoundPanel(panel13, 25);
+            PanelHelper.SetRoundPanel(panel14, 25);
+            PanelHelper.SetRoundPanel(panel15, 25);
+            PanelHelper.SetRoundPanel(panel16, 25);
+            PanelHelper.SetRoundPanel(panel17, 25);
+            PanelHelper.SetRoundPanel(panel18, 25);
+            PanelHelper.SetRoundPanel(panel19, 25);
+            PanelHelper.SetRoundPanel(panel20, 25);
+            PanelHelper.SetRoundPanel(panel21, 25);
+            PanelHelper.SetRoundPanel(panel22, 25);
+
+
+
+
         }
 
         private void Matrícula_Load(object sender, EventArgs e)
@@ -35,6 +59,36 @@ namespace SGA
         }
 
         private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mbTexbox18__TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel21_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label23_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mbTexbox17__TextChanged(object sender, EventArgs e)
         {
 
         }
