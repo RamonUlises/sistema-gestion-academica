799748685906f81abacb31418bbe2b857108590a
RamonUlises <jorgitomilina@gmail.com>
2024-05-21 20:13:57 -0600

    Connection to bd

---
+++ SGA/Connection/BD_Connection.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGA.Connection
+{
+    internal class BD_Connection
+    {
+        private MySqlConnection conexion;
+        private string server = "localhost";
+        private string database = "sistema_gestion_academica";
+        private string user = "root";
+        private string password = "57810634rrr";
+
+        private string cadenaConexion;
+
+        public BD_Connection()
+        {
+            cadenaConexion "Database=" + database + "; Data Source=" + server + "; User Id=" + user + "; Password=" + password + ";";
+        }
+        public MySqlConnecton GetConnection()
+        {
+            if(conexion == null)
+            {
+                conexion = new MySqlConnection(cadenaConexion);
+                conexion.Open();
+            }
+
+            return conexion;
+        }
+    }
+}
---
+++ SGA/Connection/DB_Connection.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGA.Connection
+{
+    internal class DB_Connection
+    {
+        private MySqlConnection conexion;
+        private string server = "localhost";
+        private string database = "sistema_gestion_academica";
+        private string user = "root";
+        private string password = "57810634rrr";
+        private string cadenaConnection;
+
+        public DB_Connection()
+        {
+            cadenaConnection = "Database=" + database + "; Data Source=" + server + "; User Id=" + user + "; Password=" + password + ";";
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            if(conexion == null)
+            {
+                conexion = new MySqlConnection(cadenaConnection);
+                conexion.Open();
+            }
+
+            return conexion;
+        }
+    }
+}
