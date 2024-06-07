9e80ca2963e56d1786e2b72a7d1c5337623684e4
RamonUlises <jorgitomilina@gmail.com>
2024-06-07 13:40:47 -0600

    message box

---
+++ SGA/MBControl/CustomMessageBox.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace SGA.MBControl
+{
+    partial class CustomMessageBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.btnNo = new SGA.MBControl.MBButton();
+            this.btnYes = new SGA.MBControl.MBButton();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.BackColor = System.Drawing.SystemColors.Window;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.ForeColor = System.Drawing.Color.Black;
+            this.lblMessage.Location = new System.Drawing.Point(25, 102);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(143, 29);
+            this.lblMessage.TabIndex = 2;
+            this.lblMessage.Text = "Hola mundo";
+            // 
+            // btnNo
+            // 
+            this.btnNo.BackColor = System.Drawing.Color.White;
+            this.btnNo.BackgroundColor = System.Drawing.Color.White;
+            this.btnNo.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(25)))), ((int)(((byte)(62)))));
+            this.btnNo.BorderRadius = 30;
+            this.btnNo.BorderSize = 0;
+            this.btnNo.FlatAppearance.BorderSize = 2;
+            this.btnNo.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnNo.ForeColor = System.Drawing.Color.Black;
+            this.btnNo.Location = new System.Drawing.Point(245, 10);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(112, 40);
+            this.btnNo.TabIndex = 1;
+            this.btnNo.Text = "No";
+            this.btnNo.TextColor = System.Drawing.Color.Black;
+            this.btnNo.UseVisualStyleBackColor = false;
+            this.btnNo.Click += new System.EventHandler(this.btnNo_Click_1);
+            // 
+            // btnYes
+            // 
+            this.btnYes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(25)))), ((int)(((byte)(62)))));
+            this.btnYes.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(25)))), ((int)(((byte)(62)))));
+            this.btnYes.BorderColor = System.Drawing.Color.White;
+            this.btnYes.BorderRadius = 30;
+            this.btnYes.BorderSize = 0;
+            this.btnYes.FlatAppearance.BorderSize = 0;
+            this.btnYes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnYes.ForeColor = System.Drawing.Color.White;
+            this.btnYes.Location = new System.Drawing.Point(363, 10);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(112, 40);
+            this.btnYes.TabIndex = 0;
+            this.btnYes.Text = "Si";
+            this.btnYes.TextColor = System.Drawing.Color.White;
+            this.btnYes.UseVisualStyleBackColor = false;
+            this.btnYes.Click += new System.EventHandler(this.btnYes_Click_1);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Gainsboro;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.Black;
+            this.lblTitle.Location = new System.Drawing.Point(25, 19);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(153, 29);
+            this.lblTitle.TabIndex = 3;
+            this.lblTitle.Text = "Hola mundo";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel1.Controls.Add(this.lblTitle);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(489, 61);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel2.Controls.Add(this.btnYes);
+            this.panel2.Controls.Add(this.btnNo);
+            this.panel2.Location = new System.Drawing.Point(0, 181);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(489, 62);
+            this.panel2.TabIndex = 5;
+            // 
+            // CustomMessageBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(487, 237);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblMessage);
+            this.Name = "CustomMessageBox";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.CustomMessageBox_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MBButton btnYes;
+        private MBButton btnNo;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
---
+++ SGA/MBControl/CustomMessageBox.cs
@@ -0,0 +1,61 @@
+﻿using Org.BouncyCastle.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SGA.MBControl
+{
+    public partial class CustomMessageBox : Form
+    {
+        public CustomMessageBox(string title, string message)
+        {
+            InitializeComponent();
+            lblTitle.Text = message;
+            lblMessage.Text = message;
+
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.ControlBox = false;
+        }
+        public static DialogResult Show(string message, string title)
+        {
+            using (CustomMessageBox customMessageBox = new CustomMessageBox(message, title))
+            {
+                return customMessageBox.ShowDialog();
+            }
+        }
+
+        private void btnYes_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnNo_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void CustomMessageBox_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnYes_Click_1(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Yes;
+            this.Close();
+        }
+
+        private void btnNo_Click_1(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.No;
+            this.Close();
+        }
+    }
+}
